c933cc1a4ddc28294162285e331ec0e4a6179656
MuthukumaranaSM <sandanimuthukumarana@gmail.com>
2025-04-26 12:59:57 +0530

    Remove unused AppService and AppController; set up Prisma with MySQL datasource and initial User model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: +configService.get('DB_PORT', 3306),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', 'root1'),
+        database: configService.get('DB_DATABASE', 'dental_management'),
+        entities: [__dirname + '/*/.entity{.ts,.js}'],
+        synchronize: configService.get('DB_SYNCHRONIZE', true),
+        logging: configService.get('DB_LOGGING', false),
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}//comment again comment
+export class AppModule {}
